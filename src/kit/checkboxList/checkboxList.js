f5e7696e7271687ce16aa0d8b553cf6ae0b5291e
bigfatmoon <a21mor@mail.ru>
2020-04-26 01:02:48 +0300

    Checkbox list update

---
+++ src/kit/checkboxList/checkboxList.js
@@ -0,0 +1,15 @@
+function init() {
+  let expander = document.querySelectorAll('.checkbox-list__expander');
+  expander.forEach(el => {
+    el.addEventListener('click', (ev) => {
+      let list = ev.currentTarget.nextSibling;
+      list.hidden = !list.hidden
+      ev.currentTarget.classList.toggle('checkbox-list__expander_opened');
+    });
+    if (!el.nextSibling.hidden) {
+      el.classList.add('checkbox-list__expander_opened');
+    }
+  })
+}
+
+export {init};
