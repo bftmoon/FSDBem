07166048962205c59ab30eb3a885e5bfacb25f3b
bigfatmoon <a21mor@mail.ru>
2020-05-06 22:30:26 +0300

    Search room adding

---
+++ src/pages/site/search-room/search-room.js
@@ -0,0 +1,6 @@
+import FloatButton from "../../../kit/blocks/float-button/FloatButton";
+
+FloatButton.initDefault({
+  elementSelector: '.js-search-page__configuration',
+  toggleClass: 'search-page__configuration_opened'
+});
