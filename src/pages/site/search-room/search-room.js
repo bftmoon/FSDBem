2e54aea769e23d9d6590b932c4a767a9ab9059bc
bigfatmoon <a21mor@mail.ru>
2020-05-07 23:57:59 +0300

    Changing float button to expander

--- src/pages/site/search-room/search-room.js
+++ src/pages/site/search-room/search-room.js
@@ -1,6 +1,9 @@
 import FloatButton from "../../../kit/blocks/float-button/FloatButton";
+import Expander from "../../../kit/blocks/expander/Expander";
 
 FloatButton.initDefault({
   elementSelector: '.js-search-page__configuration',
   toggleClass: 'search-page__configuration_opened'
 });
+
+Expander.initDefault({controlledSelector:'.js-search-page__config-content'});
