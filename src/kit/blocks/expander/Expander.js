863f90f30b5605bcd38406f59617bb048cd6db19
bigfatmoon <a21mor@mail.ru>
2020-05-07 23:55:30 +0300

    Expander adding

---
+++ src/kit/blocks/expander/Expander.js
@@ -0,0 +1,24 @@
+class Expander {
+  init($element, $controlledContent, isOpened = true) {
+    const $button = $element.find('.js-expander__button');
+    this._$icons = $button.find('.js-expander__icon');
+    this._$content = $controlledContent
+
+    this._toggleExpander = this._toggleExpander.bind(this);
+    $button.on('click', this._toggleExpander);
+
+    if (!isOpened)
+      this._toggleExpander()
+  }
+
+  _toggleExpander() {
+    this._$icons.toggle();
+    this._$content.toggle();
+  }
+
+  static initDefault({selector = '.js-expander', controlledSelector}) {
+    new Expander().init($(selector), $(controlledSelector))
+  }
+}
+
+export default Expander;
