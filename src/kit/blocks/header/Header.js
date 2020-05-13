0811c940c984b097f96971e3030cf911ee327b60
bigfatmoon <a21mor@mail.ru>
2020-05-13 22:30:16 +0300

    Header separation from it's blocks

--- src/kit/blocks/header/Header.js
+++ src/kit/blocks/header/Header.js
@@ -1,22 +1,31 @@
 class Header {
-  init($element){
+  init($element) {
+    this._handleButtonsClick = this._handleButtonsClick.bind(this);
+    this._handleDimClick = this._handleDimClick.bind(this);
+
     this._$dim = $element.find('.js-header__dim');
-    this._$sidebar=$element.find('.js-header__content');
-    const closeButton = $element.find('.js-header__closing-button');
+    this._$sidebar = $element.find('.js-header__sidebar');
+    const buttons = $element.find('.js-header__closing-button, .js-header__menu-button');
 
-    this._toggleSidebar = this._toggleSidebar.bind(this);
+    buttons.on('click', this._handleButtonsClick);
+    this._$dim.on('click', this._handleDimClick);
+  }
 
-    closeButton.on('click', this._toggleSidebar);
-    $element.find('.js-header__menu-button').on('click', this._toggleSidebar);
-    this._$dim.on('click', this._toggleSidebar);
+  _handleButtonsClick() {
+    this._toggleSidebar();
   }
-  _toggleSidebar(){
-    this._$dim.toggle();
-    console.log('jo')
-    this._$sidebar.toggleClass('header__content_opened');
+
+  _handleDimClick() {
+    this._toggleSidebar();
   }
-  static initAll(selector='.js-header'){
-    $(selector).each((_, element)=>new Header().init($(element)))
+
+  _toggleSidebar() {
+    this._$dim.toggleClass('header__dim_opened');
+    this._$sidebar.toggleClass('header__sidebar_opened');
+  }
+
+  static initAll(selector = '.js-header') {
+    $(selector).each((_, element) => new Header().init($(element)))
   }
 }
 
