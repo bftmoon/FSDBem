4e4f61d0173df5ecc8871de1dadd3411c691b59f
bigfatmoon <a21mor@mail.ru>
2020-05-13 22:33:33 +0300

    Room details and search updating

---
+++ src/pages/site/room-details/room-details.js
@@ -0,0 +1,4 @@
+import FeedbacksChart from "../../../kit/blocks/feedbacks-chart/FeedbacksChart";
+
+FeedbacksChart.initDefault({data: [130, 65, 65, 0]})
+
--- src/pages/site/search-room/search-room.js
+++ src/pages/site/search-room/search-room.js
@@ -1,9 +1,3 @@
-import FloatButton from "../../../kit/blocks/float-button/FloatButton";
 import Expander from "../../../kit/blocks/expander/Expander";
-
-FloatButton.initDefault({
-  elementSelector: '.js-search-page__configuration',
-  toggleClass: 'search-page__configuration_opened'
-});
 
 Expander.initDefault({controlledSelector:'.js-search-page__config-content'});
