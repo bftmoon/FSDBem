506a1d09d78e54c9e009dd12a487d9bee03df47e
bigfatmoon <a21mor@mail.ru>
2020-06-09 03:21:31 +0300

    Updates

--- cards~form-elements~headers-and-footers.2d79e2ddd7bcf8379ac8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{LnAa:function(n,w,o){"use strict";o("xbkG")}}]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvcGFnZXMva2l0LXBhZ2VzL2xheW91dC9sYXlvdXQuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjRGQUFBIiwiZmlsZSI6ImNhcmRzfmZvcm0tZWxlbWVudHN+aGVhZGVycy1hbmQtZm9vdGVycy4yZDc5ZTJkZGQ3YmNmODM3OWFjOC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnQHRoZW1lJztcbiJdLCJzb3VyY2VSb290IjoiIn0=
