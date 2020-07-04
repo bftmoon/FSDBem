110a147cdd9064c81f92e2d713cc6be5e42fcc70
bigfatmoon <a21mor@mail.ru>
2020-07-04 22:44:31 +0300

    #19 Header hardcoded media query point changed to js logic

--- src/kit/blocks/header/Header.js
+++ src/kit/blocks/header/Header.js
@@ -1,6 +1,38 @@
 class Header {
-  static initDefault({ menuButtonClickListener, parent = document }) {
-    parent.querySelector('.js-header__menu-button').addEventListener('click', menuButtonClickListener);
+  create($element, menuButtonClickListener) {
+    $element.find('.js-header__menu-button').on('click', menuButtonClickListener);
+
+    this.headerContent = $element.find('.js-header__content');
+    this.menuButton = this.headerContent.find('.js-header__menu-button');
+    this.items = this.headerContent.find('.js-header__items');
+    $(window).on('resize', this._handleWindowResize.bind(this));
+    this._calcMinWidth();
+    this._updateContent();
+  }
+
+  _handleWindowResize() {
+    clearTimeout(this.resizeTimer);
+    this.resizeTimer = setTimeout(() => {
+      this._updateContent();
+    }, 50);
+  }
+
+  _calcMinWidth() {
+    this.minWidth = this.headerContent.children().toArray().reduce((sum, current) => current.offsetWidth + sum, 0);
+  }
+
+  _updateContent() {
+    if (this.minWidth < this.headerContent[0].offsetWidth) {
+      this.menuButton.addClass('header__menu-button_hidden');
+      this.items.removeClass('header__items_hidden');
+    } else {
+      this.menuButton.removeClass('header__menu-button_hidden');
+      this.items.addClass('header__items_hidden');
+    }
+  }
+
+  static initDefault({menuButtonClickListener, parent = document}) {
+    new Header().create($(parent).find('.js-header:first'), menuButtonClickListener);
   }
 }
 
